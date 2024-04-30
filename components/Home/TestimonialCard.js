ac654620c14fc08c9bf00128f6f136154d666b87
JackAnderson01 <hammad.haq@launchbox.pk>
2024-05-01 03:53:25 +0500

    testimonial updated

--- components/Home/TestimonialCard.js
+++ components/Home/TestimonialCard.js
@@ -7,31 +7,34 @@ const TestimonialCard = ({ review }) => {
   const { palette } = useContext(GlobalContext);
   return (
     <div
-      className={`w-full col-span-1 flex flex-col justify-center items-start gap-3`}
+      className={`w-full min-h-64 h-64 relative  col-span-1 flex flex-col justify-center items-start gap-3`}
       style={{
         color: palette?.color,
       }}
     >
       <p
-        className="text-base"
+        className="text-base h-[80%]"
         style={{
           color: palette?.light_contrast_color,
         }}
       >
-        {review?.review}
+        {review?.review.length > 340 ? review?.review?.slice(0, 340) + "..." : review?.review}
       </p>
-      <img src={review?.image} alt="" className="rounded-full w-14 h-14" />
-      <div className="flex flex-col justify-start items-start">
-        <p className="text-base font-semibold">{review?.author}</p>
-        <p
-          className="text-sm"
-          style={{
-            color: palette?.light_contrast_color,
-          }}
-        >
-          {review?.designation}
-        </p>
+      <div className="w-auto h-[20%] absolute bottom-0 left-0 flex gap-2 justify-start items-center">
+        <img src={review?.image} alt="" className="rounded-full w-14 h-14" />
+        <div className="flex flex-col justify-start items-start">
+          <p className="text-base font-semibold">{review?.author}</p>
+          <p
+            className="text-sm"
+            style={{
+              color: palette?.light_contrast_color,
+            }}
+          >
+            {review?.designation}
+          </p>
+        </div>
       </div>
+
     </div>
   );
 };
--- constants/reviews.js
+++ constants/reviews.js
@@ -2,46 +2,54 @@
 
 export const reviews = [
   {
-    
+
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros nisl, pellentesque eu dictum sit amet, rhoncus in justo. In hac habitasse platea dictumst. Sed dictum tellus non odio mattis, consequat laoreet eros vehicula.",
-    image: "/testimonial-1.png",
-    author: "Daved James",
-    designation: "CEO, @maidsimpl",
-  },
-  {
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros nisl, pellentesque eu dictum sit amet, rhoncus in justo. In hac habitasse platea dictumst. Sed dictum tellus non odio mattis, consequat laoreet eros vehicula.",
-    image: "/testimonial-2.png",
-    author: "Daved James",
-    designation: "CEO, @maidsimpl",
-  },
-  {
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros nisl, pellentesque eu dictum sit amet, rhoncus in justo. In hac habitasse platea dictumst. Sed dictum tellus non odio mattis, consequat laoreet eros vehicula.",
+      "Dignite Studios were quick to respond to my inquiry regarding development of my app. Shane the Senior Business Consultant was amazing. He explained things in a language I could understand. They are super friendly and professional.",
     image: "/testimonial-3.png",
-    author: "Daved James",
-    designation: "CEO, @maidsimpl",
+    author: "Shantwan Humphrey",
+    designation: "CEO, @fareshare",
   },
   {
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros nisl, pellentesque eu dictum sit amet, rhoncus in justo. In hac habitasse platea dictumst. Sed dictum tellus non odio mattis, consequat laoreet eros vehicula.",
+      "Shane is an absolute pleasure to work with. Communication is on point. Him and his team always exceed my expectations!",
     image: "/testimonial-1.png",
-    author: "Daved James",
+    author: "Joshua Abely",
     designation: "CEO, @maidsimpl",
   },
   {
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros nisl, pellentesque eu dictum sit amet, rhoncus in justo. In hac habitasse platea dictumst. Sed dictum tellus non odio mattis, consequat laoreet eros vehicula.",
+      "The experience has been straight forward and seem less. I thank everyone for there knowledge and expertise on get this project done. I will be forever great full for the work that has been put in and I thank everyone for everything and all of the hard work!!",
     image: "/testimonial-2.png",
-    author: "Daved James",
-    designation: "CEO, @maidsimpl",
+    author: "Laurent Carter",
+    designation: "CEO, @carterboating",
   },
   {
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros nisl, pellentesque eu dictum sit amet, rhoncus in justo. In hac habitasse platea dictumst. Sed dictum tellus non odio mattis, consequat laoreet eros vehicula.",
-    image: "/testimonial-3.png",
-    author: "Daved James",
-    designation: "CEO, @maidsimpl",
+      "Dignite Studios have gone above and beyond my expectations. Marvin has clearly done his research into our special niche and thought of great value to add that we didn’t even think of. This is a dream come true experience and I am so thankful. I am looking forward to a very long term relationship with Dignite Studios.",
+    image: "/testimonial-4.png",
+    author: "Nicole Adcock",
+    designation: "Founder, @Divespace",
   },
+  {
+    review:
+      "I had a wonderful experience working with Liam and Maya. They were thought provoking and professional. They thought through my project in a depth no other company has. Their outstanding collaborative efforts were refreshing. I appreciate all the hard work they brought.",
+    image: "/testimonial-1.png",
+    author: "Mathew",
+    designation: "CEO, @rentalproperties",
+  },
+  {
+    review:
+      "The team has been great. I feel confident in each step that I make, including, I appreciate honest feedback and the research that my project manager Marvin does. I also love how often they gave me updates on their progress. Thank you to the whole team for making this app come to life.",
+    image: "/testimonial-3.png",
+    author: "Petula Bedeau",
+    designation: "CEO, @Loadclear",
+  },
+  {
+    review:
+      "Dignite Studios Hailey was very helpful getting my startup going, and especially patient with any difficulties I’ve presented in the process. The process was also clearly stated to me, and any questions I’ve had were answered. She even gave me some ideas for my app that were creative and showed she cared for the idea to be at its best. ",
+    image: "/testimonial-1.png",
+    author: "Chase Lassiter",
+    designation: "CEO, @Vayda",
+  },
+
 ];
