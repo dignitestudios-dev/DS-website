396f4d5106c3808f9fb5757eb3fa093947cb4275
JackAnderson01 <hammad.haq@launchbox.pk>
2024-04-30 05:23:22 +0500

    Completed

--- components/global/GlobalLoader.js
+++ components/global/GlobalLoader.js
@@ -1,10 +1,12 @@
 'use client'
 
-import React from "react";
+import { GlobalContext } from "@/context/GlobalContext";
+import React, { useContext } from "react";
 
 const GlobalLoader = () => {
+  const { palette } = useContext(GlobalContext)
   return (
-    <div className="fixed top-0 left-0 bg-white z-[1000] overflow-x-auto w-screen h-screen flex justify-center items-center ">
+    <div style={{ background: palette?.background }} className="fixed top-0 left-0 bg-white z-[1000] overflow-x-auto w-screen h-screen flex justify-center items-center ">
       <div className="loader"></div>
 
     </div>
