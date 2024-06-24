b3975aa13e36c517621978072cac79ff2c146c04
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-06-25 02:07:43 +0500

    added animation in top notch testing services page

--- components/services/TopNotchMobileAppTestingServices/TopNotchMobileAppTestingServices.js
+++ components/services/TopNotchMobileAppTestingServices/TopNotchMobileAppTestingServices.js
@@ -1,6 +1,6 @@
 "use client";
 import Navbar from "@/components/global/Navbar";
-import React, { useEffect } from "react";
+import React from "react";
 import Hero from "./Hero";
 import MobileAppTestingInRealTime from "./MobileAppTestingInRealTime";
 import TestingServices from "./TestingServices";
@@ -9,15 +9,9 @@ import TopNotchBenefits from "./TopNotchBenefits";
 import TestingTools from "./TestingTools";
 import WhyDignite from "./WhyDignite";
 import TopNotchFaqs from "./TopNotchFaqs";
-import { AOS } from "aos";
 import "aos/dist/aos.css";
 
 const TopNotchMobileAppTestingServices = () => {
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     AOS.init();
-  //   }
-  // }, []);
 
   return (
     <div className="w-full h-auto flex flex-col items-center justify-start overflow-x-hidden overflow-y-auto">
