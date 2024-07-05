0f11fd09fb3dba79abb116b1498efefd8d924937
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-06 04:15:32 +0500

    pushing the all code to test

--- components/services/MobileAppDevelopmentSeo/TopRankedCategoriesSection.jsx
+++ components/services/MobileAppDevelopmentSeo/TopRankedCategoriesSection.jsx
@@ -48,9 +48,6 @@ const TopRankedCategoriesSection = () => {
           className={`w-full rounded-[24px] p-6 flex flex-col items-start gap-3 ${
             theme === "light" ? "bg-[#F9F9F9] hover:text-white" : "bg-[#2d2d2d]"
           } hover:bg-[#F15C20] hover:text-white transition-all duration-300`}
-          // style={{
-          //   background: palette?.dark_contrast_background,
-          // }}
         >
           <h3 className={` text-xl font-semibold`}>Native Mobile App Development</h3>
           <p className={``}>
@@ -113,31 +110,6 @@ const TopRankedCategoriesSection = () => {
         </Link>
       </div>
 
-      <div className="w-full mt-12">
-        <div className="w-full lg:w-[80%] mx-auto flex flex-col lg:flex-row lg:items-end lg:justify-center gap-x-6">
-          <img
-            src="/marcio-profile.png"
-            alt=""
-            className="w-24 h-24 lg:w-auto lg:h-auto"
-          />
-          <div className="w-full lg:w-[657px]">
-            <div className="flex items-end justify-between gap-6 xl:gap-16 border border-b py-2 border-t-0 border-r-0 border-l-0">
-              <p className="uppercase text-base font-normal">
-                it is said by <span className="font-bold">Marcio Cyrillo</span>,
-                executive director at ci&t
-              </p>
-              <img src="/quotation-mark.png" alt="" />
-            </div>
-            <p className="text-xl font-normal leading-[27.197px] tracking-[-0.48px] mt-2">
-              “The panorama of our life is a myriad of adaptive applications
-              running on different devices, with different features and sensors.
-              All mobile apps collect tidbits about what we do and feed big
-              digital brains that can adapt applications to our needs simply
-              because they get to know us.”
-            </p>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
