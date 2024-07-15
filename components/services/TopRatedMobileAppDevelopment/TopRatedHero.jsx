d904544b051622676a8ca79974b88f86b18218b9
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-16 02:42:41 +0500

    mobile app dev form authentication

--- components/services/TopRatedMobileAppDevelopment/TopRatedHero.jsx
+++ components/services/TopRatedMobileAppDevelopment/TopRatedHero.jsx
@@ -11,74 +11,140 @@ import { FiArrowUpRight } from "react-icons/fi";
 import { usePathname } from "next/navigation";
 
 const TopRatedHero = () => {
-  const { palette, setIsSidebarOpen, setError, error, theme } =
+  const { palette, theme, setIsSidebarOpen, setError, error } =
     useContext(GlobalContext);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({});
+  const [countryCode, setCountryCode] = useState("");
+  const [isValid, setIsValid] = useState(true);
   const pathname = usePathname();
-  console.log("pathname >> ", pathname)
+  console.log("pathname >> ", pathname);
 
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
-  const formatPhoneNumber = (phoneNumber) => {
-    const formattedNumber = phoneNumber.replace(
-      /^(\d{3})(\d{3})(\d{4})$/,
-      "($1) $2-$3"
-    );
-    return formattedNumber;
+  const validateName = (name) => {
+    return name.length > 0;
   };
+
+  const validateMessage = (message) => {
+    return message.length > 0;
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
 
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const phone = formData.get("phone");
-    const message = formData.get("message");
-
+    const newErrors = {};
     if (name === "") {
-      setError("Name cannot be left empty.");
-    } else if (email === "") {
-      setError("Email cannot be left empty.");
+      newErrors.name = "Name cannot be left empty.";
     }
-    // else if (!validateEmail(email)) {
-    //     setError("Email must be a valid email.");
-    // }
-    else if (phone === "") {
-      setError("Phone number cannot be left empty.");
-    } else if (phone.length < 10) {
-      setError("Phone number must contain 10 numeric characters.");
-    } else if (message === "") {
-      setError("Message cannot be left empty.");
-    } else {
-      const formData = new FormData(event.target);
+    if (email === "") {
+      newErrors.email = "Email cannot be left empty.";
+    } else if (!validateEmail(email)) {
+      newErrors.email = "Email must be a valid email.";
+    }
+    if (phone.length === 0) {
+      newErrors.phone = "Phone number cannot be left empty.";
+    } else if (phone.length < 11) {
+      newErrors.phone = "Phone number cannot be less than 10 digits.";
+    } else if (phone.length > 15) {
+      newErrors.phone = "Phone number cannot be more than 15 digits.";
+    }
+    if (message === "") {
+      newErrors.message = "Message cannot be left empty.";
+    }
 
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
       const data1 = new URLSearchParams();
-
-      const formattedNumber = formatPhoneNumber(formData.get("phone"));
-      // console.log(formattedNumber);
       //Using entry ids from Google forms config
-      data1.append("entry.1883330900", formData.get("name")); // Name field
-      data1.append("entry.39421230", formData.get("email")); // Email field
-      data1.append("entry.769267793", formattedNumber); // Phone field
-      data1.append("entry.1280467825", formData.get("message")); // message field
+      data1.append("entry.1883330900", name); // Name field
+      data1.append("entry.39421230", email); // Email field
+      data1.append("entry.769267793", phone); // Phone field
+      data1.append("entry.1280467825", message); // Message field
       data1.append("entry.764492805", pathname); // Page field
+      console.log("name >> ", name);
+      console.log("email >> ", email);
+      console.log("phone >> ", phone);
+      console.log("message >> ", message);
+      console.log("pathname >> ", pathname);
 
       fetch(
         "https://docs.google.com/forms/d/e/1FAIpQLSey02yWAqdomjEVpP8CPPYgUxb0osp6uu_E6vt_47A_0X12mQ/formResponse",
         { method: "POST", body: data1, mode: "no-cors" }
       )
         .then((response) => {
+          console.log('response >> ', response)
           window.location.assign("https://www.dignitestudios.com/thank-you");
         })
         .catch((error) => {
-          setError("something went wrong");
+          setError("Something went wrong.");
         });
+    }
+  };
+
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    setName(value);
+    if (!validateName(value)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: "Name cannot be left empty.",
+      }));
+    } else {
+      setErrors((prevErrors) => {
+        const { name, ...rest } = prevErrors;
+        return rest;
+      });
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    if (!validateEmail(value)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email must be a valid email.",
+      }));
+    } else {
+      setErrors((prevErrors) => {
+        const { email, ...rest } = prevErrors;
+        return rest;
+      });
+    }
+  };
+
+  const handleMessageChange = (e) => {
+    const value = e.target.value;
+    setMessage(value);
+    if (!validateMessage(value)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        message: "Message cannot be left empty.",
+      }));
+    } else {
+      setErrors((prevErrors) => {
+        const { message, ...rest } = prevErrors;
+        return rest;
+      });
+    }
+  };
+
+  const handlePhoneChange = (value, country) => {
+    setPhone(value);
+    setCountryCode(country.dialCode);
+
+    if (!value.startsWith(`+${country.dialCode}`)) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
     }
   };
 
@@ -147,9 +213,9 @@ const TopRatedHero = () => {
               </Link>
             </div>
           </div>
-          <div className="w-full lg:w-[40%] flex items-center justify-center lg:justify-start">
+          <div className="w-full lg:w-[40%] flex items-center justify-center lg:justify-start relative">
             <div
-              className={`glass-effect h-[477px] 2xl:h-[517px] w-[95%] md:w-[397px] 2xl:w-[487px] rounded-[24px] p-6 md:px-12 md:py-14 flex flex-col justify-center items-start gap-y-10`}
+              className={`glass-effect min-h-[477px] 2xl:min-h-[517px] w-[95%] md:w-[397px] 2xl:w-[487px] rounded-[24px] p-6 md:px-12 md:py-14 flex flex-col justify-center items-start gap-y-10`}
             >
               <h1 className="uppercase font-normal text-base 2xl:text-lg text-gray-200 z-10">
                 Get in Touch With Us!
@@ -159,52 +225,95 @@ const TopRatedHero = () => {
                 onSubmit={handleSubmit}
                 className="w-full flex flex-col items-start gap-6"
               >
-                 <span className="hidden">Name:</span> <input type="text" name="name" className="hidden"/>
-         <input type="hidden" id="gclid_field" name="gclid_field" value="" className="hidden"/>
-         <input type="submit" value="Submit Form" name="btnSubmit" className="hidden"/>
-                <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
-                  <IoPerson className="text-[#F15C20] w-[16px] h-[16px]" />
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    name="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="text-[#D3D3D3] text-[13px] py-2 w-full bg-transparent border-none outline-none"
-                  />
+                <span className="hidden">Name:</span>{" "}
+                <input type="text" name="name" className="hidden" />
+                <input
+                  type="hidden"
+                  id="gclid_field"
+                  name="gclid_field"
+                  value=""
+                  className="hidden"
+                />
+                <input
+                  type="submit"
+                  value="Submit Form"
+                  name="btnSubmit"
+                  className="hidden"
+                />
+                <div className="w-full">
+                  <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
+                    <IoPerson className="text-[#F15C20] w-[16px] h-[16px]" />
+                    <input
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={handleNameChange}
+                      className="text-sm font-normal text-[#D3D3D3] outline-none w-full py-2 px-1 bg-transparent"
+                      placeholder="Enter name"
+                    />
+                  </div>
+                  {errors.name && (
+                    <span className="text-red-500 text-sm">{errors.name}</span>
+                  )}
                 </div>
-                <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
-                  <MdEmail className="text-[#F15C20] w-[16px] h-[16px]" />
-                  <input
-                    type="text"
-                    placeholder="Email address"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="text-[#D3D3D3] text-[13px] py-2 w-full bg-transparent border-none outline-none"
-                  />
+                <div className="w-full">
+                  <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
+                    <MdEmail className="text-[#F15C20] w-[16px] h-[16px]" />
+                    <input
+                      type="email"
+                      name="email"
+                      value={email}
+                      onChange={handleEmailChange}
+                      className="text-sm font-normal text-[#D3D3D3] outline-none w-full py-2 px-1 bg-transparent"
+                      placeholder="Enter email address"
+                    />
+                  </div>
+                  {errors.email && (
+                    <span className="text-red-500 text-sm">{errors.email}</span>
+                  )}
                 </div>
-                <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
-                  <FaPhone className="text-[#F15C20] w-[16px] h-[16px]" />
-                  <input
-                    type="text"
-                    placeholder="Phone number"
-                    name="phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    className="text-[#D3D3D3] text-[13px] py-2 w-full bg-transparent border-none outline-none"
-                  />
+                <div className="w-full">
+                  <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
+                    <FaPhone className="text-[#F15C20] w-[16px] h-[16px]" />
+                    <input
+                      type="text"
+                      placeholder="Phone number"
+                      name="phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      className="text-[#D3D3D3] text-[13px] py-2 w-full bg-transparent border-none outline-none"
+                    />
+                  </div>
+                  {errors.phone && (
+                    <span className="text-red-500 text-sm">{errors.phone}</span>
+                  )}
                 </div>
-                <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
-                  <MdEmail className="text-[#F15C20] w-[16px] h-[16px]" />
-                  <input
+                <div className="w-full">
+                  <div className="w-full border border-b border-t-0 border-r-0 border-l-0 flex items-center justify-start gap-2">
+                    <MdEmail className="text-[#F15C20] w-[16px] h-[16px]" />
+                    {/* <input
                     type="text"
                     placeholder="Message"
                     name="message"
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
                     className="text-[#D3D3D3] text-[13px] py-2 w-full bg-transparent border-none outline-none"
-                  />
+                  /> */}
+                    <input
+                      type="text"
+                      name="message"
+                      value={message}
+                      onChange={handleMessageChange}
+                      className="text-sm font-normal text-[#D3D3D3] outline-none w-full py-2 px-1 bg-transparent"
+                      placeholder="Type here"
+                      // style={{ borderBottom: "1px solid silver" }}
+                    />
+                  </div>
+                  {errors.message && (
+                    <span className="text-red-500 text-sm">
+                      {errors.message}
+                    </span>
+                  )}
                 </div>
                 <div className="w-full mt-3">
                   <button
