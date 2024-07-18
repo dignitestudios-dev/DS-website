bc707fa64460844ba9d230ba9a682d0a733ea82f
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-19 01:57:44 +0500

    add alt tags and titles in home page convenient section

--- components/Home/MobileAppServiceCard.jsx
+++ components/Home/MobileAppServiceCard.jsx
@@ -10,6 +10,7 @@ const MobileAppServiceCard = () => {
   const [showMore4, setShowMore4] = useState(false);
   const [showMore5, setShowMore5] = useState(false);
   const [showMore6, setShowMore6] = useState(false);
+  const [showMore7, setShowMore7] = useState(false);
 
   const handleShowMore = () => {
     setShowMore(!showMore);
@@ -29,13 +30,19 @@ const MobileAppServiceCard = () => {
   const handleShowMore6 = () => {
     setShowMore6(!showMore6);
   };
+  const handleShowMore7 = () => {
+    setShowMore7(!showMore7);
+  };
 
   const paragraphFull = (
     <>
       Mobile application development is a fast-paced emerging field of new
       trends and the latest deployment strategies to boost business in the
       digital world. Whereas, our{" "}
-      <Link href="/services/mobile-app-development-services" className="text-orange-600">
+      <Link
+        href="/services/mobile-app-development-services"
+        className="text-orange-600"
+      >
         mobile app development services
       </Link>{" "}
       produce different types of mobile applications such as basic apps, complex
@@ -80,7 +87,7 @@ const MobileAppServiceCard = () => {
         href="/services/android-app-development-services"
         className="text-orange-600"
       >
-        Android app development service
+        android app development service
       </Link>{" "}
       ? At the door of skilled mobile app development experts, you can establish
       your app with modern tools like Android Studio and Software Development
@@ -100,7 +107,7 @@ const MobileAppServiceCard = () => {
     <>
       Are you worried about your{" "}
       <Link href="/services/web-app-development" className="text-orange-600">
-        web application development services
+        web app development services
       </Link>{" "}
       ? No need to, our valuable and reliable web application team can help you
       to create the application process on remote servers and also on any medium
@@ -121,7 +128,10 @@ const MobileAppServiceCard = () => {
       skillful and dedicated team can make your mobile applications flawless and
       innovative UI/UX design to create engaging visual elements in mobile
       applications. Therefore, we assure the best{" "}
-      <Link href="/services/mobile-app-design-services" className="text-orange-600">
+      <Link
+        href="/services/mobile-app-design-services"
+        className="text-orange-600"
+      >
         mobile app design services
       </Link>{" "}
       in the USA.
@@ -144,7 +154,7 @@ const MobileAppServiceCard = () => {
         href="/services/mobile-app-testing-services"
         className="text-orange-600"
       >
-        mobile application testing services
+        mobile app testing services
       </Link>{" "}
       must ensure the removal of bugs and security issues to adhere to its
       worthiness. And, so Dignite Studios will make your mobile application
@@ -161,6 +171,34 @@ const MobileAppServiceCard = () => {
       170
     ) + "...";
 
+  const paragraphFull7 = (
+    <>
+      Forming a mobile application is a great way for business owners to improve
+      their marketing strategy, service optimization, and cost-effectiveness. It
+      can systematically increase your revenue and click-through rate by
+      consulting it with reliable mobile app developers. Therefore, each mobile
+      app has its aspects and functionality in terms of target audience, design,
+      monetization, security, and privacy. However, it needs to be evaluated
+      foremost from a better platform. So, at the door of Dignite Studios, you
+      can avail the best{" "}
+      <Link
+        href="/services/mobile-app-consulting-services"
+        className="text-orange-600"
+      >
+        mobil app consulting services
+      </Link>{" "}
+      to provide you with free consultation on your mobile app development.
+      Also, our experts are available 24/7 to assist you on call. So, don’t wait
+      and upgrade your mobile devices.
+    </>
+  );
+
+  const paragraphShort7 =
+    "Forming a mobile application is a great way for business owners to improve their marketing strategy, service optimization, and cost-effectiveness. It can systematically increase your revenue and click-through rate by consulting it with reliable mobile app developers. Therefore, each mobile app has its aspects and functionality in terms of target audience, design, monetization, security, and privacy. However, it needs to be evaluated foremost from a better platform. So, at the door of Dignite Studios, you can avail the best mobile consulting services to provide you with free consultation on your mobile app development. Also, our experts are available 24/7 to assist you on call. So, don’t wait and upgrade your mobile devices....".substring(
+      0,
+      170
+    ) + "...";
+
   return (
     <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-6">
       <div
@@ -171,8 +209,9 @@ const MobileAppServiceCard = () => {
         }`}
       >
         <img
-          src="/top-mobile-app-development-services.png"
-          alt=""
+          src="/top-mobile-app-development-services.webp"
+          title="top mobile app development services"
+          alt="image shows man is holding a mobile device that appears top mobile app development layout"
           className="w-full h-[200px] rounded-xl"
         />
         <div className="flex flex-col items-start text-start gap-1 pt-3">
@@ -205,7 +244,7 @@ const MobileAppServiceCard = () => {
         }`}
       >
         <img
-          src="/home-ios-development.png"
+          src="/home-ios-development.webp"
           alt=""
           className="w-full h-[200px] rounded-xl"
         />
@@ -239,7 +278,7 @@ const MobileAppServiceCard = () => {
         }`}
       >
         <img
-          src="/home-android-app-development.png"
+          src="/home-android-app-development.webp"
           alt=""
           className="w-full h-[200px] rounded-xl"
         />
@@ -273,7 +312,7 @@ const MobileAppServiceCard = () => {
         }`}
       >
         <img
-          src="/home-web-app-development.png"
+          src="/home-web-app-development.webp"
           alt=""
           className="w-full h-[200px] rounded-xl"
         />
@@ -307,8 +346,9 @@ const MobileAppServiceCard = () => {
         }`}
       >
         <img
-          src="/top-mobile-app-design-services.png"
-          alt=""
+          src="/top-mobile-app-design-services.webp"
+          title="top mobile app design services"
+          alt="image shows man is holding a mobile device that appears top mobile app design interface"
           className="w-full h-[200px] rounded-xl"
         />
         <div className="flex flex-col items-start text-start gap-1 pt-3">
@@ -341,8 +381,9 @@ const MobileAppServiceCard = () => {
         }`}
       >
         <img
-          src="/top-mobile-app-development-testing-services.png"
-          alt=""
+          src="/top-mobile-app-development-testing-services.webp"
+          title="top mobile app development testing services"
+          alt="image shows man is holding a mobile device that appears top mobile app testing screen"
           className="w-full h-[200px] rounded-xl"
         />
         <div className="flex flex-col items-start text-start gap-1 pt-3">
@@ -362,6 +403,41 @@ const MobileAppServiceCard = () => {
               }`}
             >
               {showMore6 ? "Show Less" : "Show More"}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`w-full min-h-[329px] rounded-xl p-3 ${
+          theme === "light"
+            ? "border border-[#E7E7E7]"
+            : "border border-[#7c7c7c]"
+        }`}
+      >
+        <img
+          src="/top-mobile-app-development-consulting-services.webp"
+          title="top mobile app development consulting services"
+          alt="image shows a mobile app consulting discussion between consultant and client"
+          className="w-full h-[200px] rounded-xl"
+        />
+        <div className="flex flex-col items-start text-start gap-1 pt-3">
+          <h1 className="text-base font-semibold">Mobile App Consulting</h1>
+          <div className="flex flex-wrap gap-1">
+            <p
+              className={`text-[13px] ${
+                theme === "light" ? "text-[#5C5C5C]" : "text-[#9d9d9d]"
+              }`}
+            >
+              {showMore7 ? paragraphFull7 : paragraphShort7}
+            </p>
+            <button
+              onClick={handleShowMore7}
+              className={`text-[13px] font-semibold ${
+                theme === "light" ? "text-[#F15C20]" : "text-[#F15C20]"
+              }`}
+            >
+              {showMore7 ? "Show Less" : "Show More"}
             </button>
           </div>
         </div>
