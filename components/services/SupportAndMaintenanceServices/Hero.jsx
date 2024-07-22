1fcb8d244b310daed0490d9f2b5c061f6fb8cd40
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-22 23:56:16 +0500

    changed font family

--- components/services/SupportAndMaintenanceServices/Hero.jsx
+++ components/services/SupportAndMaintenanceServices/Hero.jsx
@@ -5,7 +5,7 @@ import React, { useContext } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
 
 const Hero = () => {
-  const { palette } = useContext(GlobalContext);
+  const { palette, theme } = useContext(GlobalContext);
   return (
     <main className="w-full  flex flex-col pt-2 md:pt-10  gap-4 justify-start items-center">
       <h1
@@ -17,11 +17,11 @@ const Hero = () => {
         Robust Your Mobile Application <br />{" "}
         <span className="text-[#F15C20]">Support and Maintenance</span> Services
       </h1>
-      <h2 className={`text-base font-normal ${''}`}>Get free maintenance and support service from our top experts.</h2>
+      <h2 className={`text-base font-normal text-[#5C5C5C]`}>Get free maintenance and support service from our top experts.</h2>
       <div>
         <Link
           href={"/contact-us"}
-          className="bg-[#F15C20] text-white w-[164px] h-[64px] rounded-full text-center"
+          className="bg-[#F15C20] text-white w-[164px] h-[64px] rounded-full text-center flex items-center gap-2 justify-center font-semibold"
         >
           Get a quote <FiArrowUpRight className="text-2xl" />
         </Link>
--- components/services/SupportAndMaintenanceServices/SupportAndMaintenanceServicesPage.js
+++ components/services/SupportAndMaintenanceServices/SupportAndMaintenanceServicesPage.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Hero from './Hero'
+'use client'
+
+import React, { useContext } from "react";
+import Hero from "./Hero";
+import { GlobalContext } from "@/context/GlobalContext";
+import Navbar from "@/components/global/Navbar";
 
 const SupportAndMaintenanceServicesPage = () => {
+  const { theme } = useContext(GlobalContext);
   return (
-    <div className='w-full overflow-x-hidden overflow-y-auto'>
-      <Hero/>
+    <div className="w-full overflow-x-hidden overflow-y-auto">
+      <Navbar />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default SupportAndMaintenanceServicesPage
+export default SupportAndMaintenanceServicesPage;
