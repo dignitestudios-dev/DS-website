f375aafd18d72e981d53d2821aaeaa22cde37b4d
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-23 01:47:49 +0500

    changed robots.txt file

--- components/services/SupportAndMaintenanceServices/MaintenanceProcess.jsx
+++ components/services/SupportAndMaintenanceServices/MaintenanceProcess.jsx
@@ -1,8 +1,12 @@
+'use client'
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
+import { MAINTENANCE_PROCESS } from "@/constants/maintenanceprocess";
+import { GlobalContext } from "@/context/GlobalContext";
 
 const MaintenanceProcess = () => {
+    const {theme} = useContext(GlobalContext);
   return (
     <section className="w-full py-12 global-padding-horizontal">
       <section className="w-full grid grid-cols-1 lg:grid-cols-3 gap-y-6">
@@ -30,6 +34,59 @@ const MaintenanceProcess = () => {
           </div>
         </div>
       </section>
+
+      <div className="w-full lg:mt-6 lg:py-2 grid grid-cols-1 gap-2 lg:gap-0 md:grid-cols-2 xl:grid-cols-3 md:grid-rows-3 lg:grid-rows-2 xl:mt-10">
+        {MAINTENANCE_PROCESS.map((service, key) => {
+          return (
+            <div
+            key={key}
+              className={`group transition-all duration-300 border-[1px] ${
+                theme == "dark" ? "border-[#393939]" : "border-[#e7e7e7]"
+              } lg:border-0 rounded-3xl lg:rounded-none border-collapse flex flex-col ${
+                theme == "dark" ? "border-[#1c1c1c]" : "border-gray-200"
+              } justify-start items-start gap-2 p-4 md:py-3 md:px-3 lg:p-8 hover:bg-[#F15C20]  transition-all duration-150 ${
+                key == 0
+                  ? "lg:border-b "
+                  : key == 1
+                  ? "lg:border-b lg:border-r-0 xl:border-r lg:border-l "
+                  : key == 2
+                  ? "lg:border-b lg:border-r xl:border-r-0"
+                  : key === 3
+                  ? ""
+                  : key == 4
+                  ? "lg:border-r lg:border-l-0 xl:border-l"
+                  : key == 5
+                  ? ""
+                  : ""
+              }`}
+            >
+              <div className="w-full flex flex-col gap-2 md:gap-4 justify-start items-start">
+                <p className="text-[40px] font-extrabold text-[#B4B4B499]">
+                  {service.count}
+                </p>
+                <h1
+                  className={`font-bold text-[28px] ${
+                    theme == "dark"
+                      ? "text-white group-hover:text-white"
+                      : "text-black group-hover:text-white"
+                  }`}
+                >
+                  {service.title}
+                </h1>
+                <p
+                  className={`text-sm leading-[19.14px] ${
+                    theme == "dark"
+                      ? "text-gray-200 group-hover:text-gray-200"
+                      : "text-[#8f8f8f] group-hover:text-gray-200"
+                  }`}
+                >
+                  {service.desc}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
---
+++ constants/maintenanceprocess.js
@@ -0,0 +1,32 @@
+export const MAINTENANCE_PROCESS = [
+  {
+    count: "01",
+    title: "Leading Valuation",
+    desc: "It refers to the evaluation process for the mobile app idea that leverages flawless UX design, navigation, and development that enhance your app's user retention to make it more effective. By 2025, the number of smartphone users will likely cross 311 million, presenting an enormous opportunity for developers. The well-established service of Dignite Studios creates strong evaluations of your app’s worthiness with proper mobile app maintenance framework and budgeting before eloquent towards the development phase.",
+  },
+  {
+    count: "02",
+    title: "Service Planning",
+    desc: "The glamour of ideation depends on the rigorous effort of mobile app development planning to accomplish the successful app requirement. The experts of Dignite Studios develop tailor-made strategies and maintenance packages of eradicating bugs, and security rejuvenation to protect advanced features throughout the process.",
+  },
+  {
+    count: "03",
+    title: "Monitoring and Compliance Management",
+    desc: "Apps are also subject to  laws and guidelines under specific standards and protocols. Whereas, compliance testing is a broad category of mobile app testing that cynosures to meet legal exigencies of the apps during mobile application maintenance. We create constructive procedures in identifying potential issues and maintaining app compliance before they brunt users",
+  },
+  {
+    count: "04",
+    title: "Scheduled Maintenance",
+    desc: "It spotlights the regular update maintenance to clinch smooth management by the team. Our mobile app support and maintenance services ensure seamless mobile app maintenance with the latest updates of operating systems, software features, libraries, and monitor security consummation. ",
+  },
+  {
+    count: "05",
+    title: "Performance Tracking and Mobile Testing",
+    desc: "Our top-notch services establish fast responsiveness and sleek user experience to identify performance and ensure strategic mobile testing.",
+  },
+  {
+    count: "06",
+    title: "User Feedback Integration",
+    desc: "Dignite’s application services originate sharp concentration on valuable feedback and reviews on support and maintenance of the mobile app. We integrate valuable insights for app enhancement to ensure reliable services from our experts.",
+  },
+];
