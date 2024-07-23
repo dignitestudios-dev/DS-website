7922096d21fdaee3cc6db597c60e647d3858c727
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-24 04:26:06 +0500

    home page changes and web app dev service page donw

--- constants/WebAppDevChooseUs.js
+++ constants/WebAppDevChooseUs.js
@@ -2,21 +2,21 @@ export const WEB_APP_DEV_CHOOSE_US = [
     {
       title: "Quality Assurance",
       description: "Quality is our pivot focal point. The exclusive web application development services of Dignite Studios ensure high-quality standards under rigorous development protocols through testing of functions, operations, and fast performance of the web app. The company also bestows app maintenance to evaluate software updates and rising threats.",
-      icon: "/mobile-app-maintenance-expert-developers-icon.webp",
+      icon: "/quality-assurance.png",
       alt: "",
       image_title:""
     },
     {
       title: "Reliability",
       description: "The industry-leading company Dignite Studios always transcends its commitments towards its partners and end-users. We construct valuable strategies to provide culminating solutions for customer service apps Our track record is a testament to our reliability.",
-      icon: "/mobile-app-support-and-maintenance-track-record-of-bugs-fixing-icon.webp",
+      icon: "/reliablity.png",
       alt: "",
       image_title:""
     },
     {
       title: "Exponential Innovation",
       description: "Discover the creative services of Dignite Studios, which always strives to produce innovative software solutions and frameworks for clients. We always dig into modern development tools and technologies to deliver exclusive web projects as per the user's needs.",
-      icon: "/mobile-app-and-support-maintenance-exceptional-performance-icon.webp",
+      icon: "/exponential-innovation.png",
       alt: "",
       image_title:""
     }
