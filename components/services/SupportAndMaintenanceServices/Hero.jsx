a9bb7add9652682c309019568c7fd31728b3e108
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-24 00:38:12 +0500

    completed maintenace and support page

--- components/services/SupportAndMaintenanceServices/Hero.jsx
+++ components/services/SupportAndMaintenanceServices/Hero.jsx
@@ -36,26 +36,34 @@ const Hero = () => {
         <img
           src="/maintenance-hero-borders.webp"
           alt=""
-          className="w-full lg:w-[934] xl:h-[639.79px] xl:w-[1134.56px] mx-auto absolute top-0 md:translate-x-[2%] xl:translate-x-[10%] z-0"
+          className="w-full lg:w-[934] xl:h-[639.79px] xl:w-[1134.56px] mx-auto absolute top-0 md:translate-x-[2%] xl:translate-x-[7%] 2xl:translate-x-[20%] z-0"
         />
         <div className="w-full flex items-center justify-center z-10 mt-10 gap-3 md:gap-6 relative">
-          <img src="/maintenance-hero-left-icon.png" alt="" className="w-[157.05px] h-[100px] hidden lg:block absolute top-9 -left-20 xl:left-24 z-20 animated-icon-3"/>
+          <img
+            src="/maintenance-hero-left-icon.png"
+            alt=""
+            className="w-[157.05px] h-[100px] hidden lg:block absolute top-9 -left-20 xl:left-24 2xl:left-32 z-20 animated-icon-3"
+          />
           <img
             src="/support-header-img1.png"
             alt=""
-            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] z-0 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative animated-image-up"
+            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] 2xl:h-[550px] 2xl:w-[310px] z-0 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative animated-image-up"
           />
           <img
             src="/support-header-img2.png"
             alt=""
-            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] z-20 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative -top-5 animated-image-down2 hidden lg:block"
+            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] 2xl:h-[550px] 2xl:w-[310px] z-20 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative -top-5 animated-image-down2 hidden lg:block"
           />
           <img
             src="/support-header-img3.png"
             alt=""
-            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] z-20 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative -top-5 animated-image-down"
+            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] 2xl:h-[550px] 2xl:w-[310px] z-20 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative -top-5 animated-image-down"
           />
-          <img src="/app-maintenance-icon.png" alt="" className="w-[187.05px] h-[107px] hidden lg:block absolute bottom-16 -right-28 xl:right-20 z-20 animated-icon"/>
+          <img
+            src="/app-maintenance-icon.png"
+            alt=""
+            className="w-[187.05px] h-[107px] hidden lg:block absolute bottom-16 -right-28 xl:right-20 2xl:right-28 2xl:bottom-24 z-20 animated-icon"
+          />
         </div>
       </div>
     </main>
