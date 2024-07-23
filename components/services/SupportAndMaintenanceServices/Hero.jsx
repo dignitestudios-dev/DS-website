1b04f6483249fbb10be994a95504b0c07ad4d870
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-24 00:25:27 +0500

    completed maintenace and support page

--- components/services/SupportAndMaintenanceServices/Hero.jsx
+++ components/services/SupportAndMaintenanceServices/Hero.jsx
@@ -7,7 +7,7 @@ import { FiArrowUpRight } from "react-icons/fi";
 const Hero = () => {
   const { palette, theme } = useContext(GlobalContext);
   return (
-    <main className="w-full global-padding-horizontal flex flex-col pt-2 md:py-10  gap-4 justify-start items-center">
+    <main className="w-full global-padding-horizontal flex flex-col pt-2 md:py-10 h-[110vh] lg:h-[140vh] 2xl:h-screen relative gap-4 justify-start items-center">
       <h1
         style={{
           color: palette?.color,
@@ -32,14 +32,32 @@ const Hero = () => {
           Get A Quote <FiArrowUpRight className="text-2xl" />
         </Link>
       </div>
-      {/* <div className="w-full relative">
-        <img src="/maintenance-hero-borders.webp" alt="" className="xl:w-[1134.56px] mx-auto absolute top-0 translate-x-[20%] z-0"/>
-        <div className="w-full flex items-center justify-center z-10 mt-10 relative">
-            <img src="/maintenance-hero-mockup-1.png" alt="" className="w-[300px] h-[500.1px] z-20"/>
-            <img src="/maintenance-hero-mockup-2.png" alt="" className="w-[300px] h-[500.1px] z-20"/>
-            <img src="/maintenance-hero-mockup-3.png" alt="" className="w-[300px] h-[500.1px] z-20"/>
+      <div className="w-full relative">
+        <img
+          src="/maintenance-hero-borders.webp"
+          alt=""
+          className="w-full lg:w-[934] xl:h-[639.79px] xl:w-[1134.56px] mx-auto absolute top-0 md:translate-x-[2%] xl:translate-x-[10%] z-0"
+        />
+        <div className="w-full flex items-center justify-center z-10 mt-10 gap-3 md:gap-6 relative">
+          <img src="/maintenance-hero-left-icon.png" alt="" className="w-[157.05px] h-[100px] hidden lg:block absolute top-9 -left-20 xl:left-24 z-20 animated-icon-3"/>
+          <img
+            src="/support-header-img1.png"
+            alt=""
+            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] z-0 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative animated-image-up"
+          />
+          <img
+            src="/support-header-img2.png"
+            alt=""
+            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] z-20 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative -top-5 animated-image-down2 hidden lg:block"
+          />
+          <img
+            src="/support-header-img3.png"
+            alt=""
+            className="w-[200px] xl:w-[260px] h-[300px] md:h-[400px] xl:h-[500.1px] z-20 p-3 maintenance-mockup-shadow bg-white rounded-3xl relative -top-5 animated-image-down"
+          />
+          <img src="/app-maintenance-icon.png" alt="" className="w-[187.05px] h-[107px] hidden lg:block absolute bottom-16 -right-28 xl:right-20 z-20 animated-icon"/>
         </div>
-      </div> */}
+      </div>
     </main>
   );
 };
