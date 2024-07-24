0897c34dbf64a6c03134d00af5d0c58c2be5287d
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-25 00:35:33 +0500

    web app dev service page animation done

--- components/services/WebAppDevelopmentServices/Hero.jsx
+++ components/services/WebAppDevelopmentServices/Hero.jsx
@@ -7,32 +7,66 @@ import { FiArrowUpRight } from "react-icons/fi";
 const Hero = () => {
   const { palette, theme } = useContext(GlobalContext);
   return (
-    <main className="w-full global-padding-horizontal flex flex-col pt-2 md:py-10 relative gap-4 justify-start items-center">
-      <h1
-        style={{
-          color: palette?.color,
-        }}
-        className="text-[1.8rem] lg:text-6xl xl:text-[68px] 2xl:text-[94px] font-semibold xl:font-bold tracking-tighter text-center capitalize lg:leading-[71.93px] 2xl:leading-[99px]"
-      >
-        Purpose-Driven {" "}
-        <span className="text-[#F15C20]">Web Application</span> Development Services
-      </h1>
-      <h2
-        className={`text-base font-normal text-center ${
-          theme === "light" ? "text-[#5C5C5C]" : "text-[#8F8F8F]"
-        }`}
-      >
-        Your idea, our solution.
-      </h2>
-      <div>
-        <Link
-          href={"/contact-us"}
-          className="bg-[#F15C20] text-white w-[164px] h-[64px] rounded-full text-center flex items-center gap-1 justify-center font-semibold"
+    <main className="w-full ">
+      <div className="w-full global-padding-horizontal flex flex-col pt-2 md:py-10 relative gap-4 justify-start items-center">
+        <h1
+          style={{
+            color: palette?.color,
+          }}
+          className="text-[1.8rem] lg:text-6xl xl:text-[68px] 2xl:text-[94px] font-semibold xl:font-bold tracking-tighter text-center capitalize lg:leading-[71.93px] 2xl:leading-[99px]"
         >
-          Get A Quote <FiArrowUpRight className="text-2xl" />
-        </Link>
+          Purpose-Driven <span className="text-[#F15C20]">Web Application</span>{" "}
+          Development Services
+        </h1>
+        <h2
+          className={`text-base font-normal text-center ${
+            theme === "light" ? "text-[#5C5C5C]" : "text-[#8F8F8F]"
+          }`}
+        >
+          Your idea, our solution.
+        </h2>
+        <div>
+          <Link
+            href={"/contact-us"}
+            className="bg-[#F15C20] text-white w-[164px] h-[64px] rounded-full text-center flex items-center gap-1 justify-center font-semibold"
+          >
+            Get A Quote <FiArrowUpRight className="text-2xl" />
+          </Link>
+        </div>
       </div>
-      
+
+      <div className="w-full mt-6 md:mt-0 relative lg:h-[60vh] xl:h-[80vh] flex items-center justify-center">
+        <img
+          src="/Web-Application-Development-Services-Left.png"
+          alt=""
+          className="lg:w-[346px] lg:h-[325px] 2xl:w-[426px] 2xl:h-[395px] absolute top-16 2xl:top-44 left-14 2xl:left-20 z-20 slide-right-on-load hidden xl:block"
+        />
+        <img
+          src="/Web-Application-Development-Services-Left-Bottom.png"
+          alt=""
+          className="lg:w-[450px] lg:h-[358px] 2xl:w-[530px] 2xl:h-[418px] absolute top-1/2 left-24 2xl:left-36 transform -translate-y-3/5 2xl:-translate-y-[-40px] z-20 slide-right-on-load hidden xl:block"
+        />
+        <img
+          src="/web-app-dev-image-shadow-purple.png"
+          alt=""
+          className="w-[80%] lg:h-[78vh] xl:h-[82vh] absolute inset-0 m-auto z-0 fade-in"
+        />
+        <img
+          src="/web-app-dev-image-shadow-yellow.png"
+          alt=""
+          className="w-[80%] lg:h-[78vh] xl:h-[82vh] absolute inset-0 m-auto z-0 fade-out"
+        />
+        <img
+          src="/Web-Application-Development-Services.png"
+          alt=""
+          className="w-[90%] xl:w-[834px] lg:w-[60%] lg:h-[350px] xl:h-[520px] 2xl:w-[1034px] 2xl:h-[690px] relative mx-auto border web-app-dev-hero-mockup z-10 rounded-2xl animated-shadow"
+        />
+        <img
+          src="/Web-Application-Development-Services-Right.png"
+          alt=""
+          className="hidden xl:block lg:w-[317px] lg:h-[231px] 2xl:w-[407px] 2xl:h-[311px] absolute top-1/2 right-20 2xl:right-24 transform -translate-y-1/2 z-20 slide-left-on-load"
+        />
+      </div>
     </main>
   );
 };
--- components/services/WebAppDevelopmentServices/UnrivaledSection.jsx
+++ components/services/WebAppDevelopmentServices/UnrivaledSection.jsx
@@ -4,9 +4,11 @@ import { FiArrowUpRight } from "react-icons/fi";
 
 const UnrivaledSection = () => {
   return (
-    <section className="w-full global-padding-horizontal py-12 grid grid-cols-1 lg:grid-cols-2 gap-y-6">
-      <div className="flex flex-col items-start text-start gap-y-3">
-        <h1 className="text-xl font-bold text-[#8F8F8F] uppercase">Unrivaled</h1>
+    <section className="w-full py-12 flex items-end flex-col lg:flex-row gap-y-6">
+      <div className="flex global-padding-horizontal flex-col items-start text-start gap-y-3">
+        <h1 className="text-xl font-bold text-[#8F8F8F] uppercase">
+          Unrivaled
+        </h1>
         <h2 className="headings-size">
           <span className="text-[#F15C20]">Web App</span> Development Company
         </h2>
@@ -29,7 +31,24 @@ const UnrivaledSection = () => {
           </Link>
         </div>
       </div>
-      <div></div>
+      <img
+        src="/Web-app-development-company.png"
+        alt=""
+        className="lg:w-[826px] lg:h-[573px] float-end slide-left-on-load"
+        data-aos="slide-left"
+        data-aos-offset="100" // Adjusted offset to trigger animation earlier
+        data-aos-delay="0" // No delay for immediate animation start
+        data-aos-duration="500"
+        data-aos-easing="ease-in"
+        data-aos-mirror="false"
+        data-aos-once="false" // Set to false to repeat animation on scroll up
+        data-aos-anchor-placement="center-bottom"
+      />
+      {/* <div className="flex items-center justify-end border">
+        <div>
+         
+        </div> */}
+      {/* </div> */}
     </section>
   );
 };
