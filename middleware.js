275e81e0955645d1832c86cecce81bd89d49b423
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-07-27 06:07:30 +0500

    updated robots.txt

--- middleware.js
+++ middleware.js
@@ -1,13 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  if (request.nextUrl.pathname === "/flutter-gray.webp") {
-    return NextResponse.redirect(
-      new URL(
-        "/Mobile-app-development-technology-Flutter-Icon-color.webp",
-        request.url
-      )
-    );
+  const { pathname } = request.nextUrl;
+
+  // Define an array of paths that should be redirected
+  const pathsToRedirect = [
+    '/flutter-gray.webp',
+    '/flutter-color.webp'
+  ];
+
+  // Check if the requested path matches any of the paths to redirect
+  if (pathsToRedirect.includes(pathname)) {
+    // Redirect to the home page (or any other URL)
+    return NextResponse.redirect(new URL('/', request.url));
   }
+
+  // Allow other requests to proceed as normal
   return NextResponse.next();
 }
