f1481e77709ff6793392bc1e7d4e1c5fbe817534
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-08-17 03:44:48 +0500

    changed image name on contact us page

--- constants/industries.js
+++ constants/industries.js
@@ -1,6 +1,6 @@
 export const industries = [
   {
-    image: "/mobile-App-development-telemdicine-industry-icon.webp",
+    image: "/mobile-App-development-telemdicine-industry-icon-1.webp",
     title: "Lifestyle",
     description:
       "Mobile applications designed to make your daily life easier and more convenient. They cover a wide range of areas, from helping you stay healthy and organized to finding new restaurants and exploring your city.",
@@ -9,7 +9,7 @@ export const industries = [
     alt: "Mobile App Development Telemdicine Industry Icon",
   },
   {
-    image: "/mobile-App-development-fintech-industry-icon.webp",
+    image: "/mobile-App-development-fintech-industry-icon-1.webp",
     title: "Ride Sharing",
     description:
       "Mobile applications that connect passengers with drivers for on-demand transportation. They are a popular alternative to traditional taxis and public transportation, offering a convenient and often more affordable way to get around.",
@@ -18,7 +18,7 @@ export const industries = [
     alt: "Mobile App Development Telemdicine Fintech Icon",
   },
   {
-    image: "/mobile-App-development-dating-industry-icon.webp",
+    image: "/mobile-App-development-dating-industry-icon-1.webp",
     title: "Dating Apps",
     description:
       "Mobile applications designed to connect people for romantic interests or casual encounters. They function by creating profiles for users and utilizing algorithms to match them with compatible individuals based on various criteria.",
@@ -27,7 +27,7 @@ export const industries = [
     alt: "Mobile App Development Dating Industry Icon",
   },
   {
-    image: "/mobile-App-development-ecommerce-industry-icon.webp",
+    image: "/mobile-App-development-ecommerce-industry-icon-1.webp",
     title: "E-Commerce",
     description:
       "Mobile applications designed specifically for users to browse and purchase products from online stores through their mobile devices. They offer a convenient and accessible way to shop anytime, anywhere, contributing to the ever-growing trend of mobile shopping.",
@@ -36,7 +36,7 @@ export const industries = [
     alt: "Mobile App Development E-Commerce Industry Icon",
   },
   {
-    image: "/mobile-app-development-social-media-industry-icon.webp",
+    image: "/mobile-app-development-social-media-industry-icon-1.webp",
     title: "Social Media",
     description:
       "Mobile applications that enable users to share content & interact with friends and family, and stay up-to-date on current events. They have become an integral part of many people's lives, providing a platform for connection, communication, and entertainment.",
@@ -45,7 +45,7 @@ export const industries = [
     alt: "Mobile App Development Social Industry Icon",
   },
   {
-    image: "/mobile-app-development-merketplace-industry-icon.webp",
+    image: "/mobile-app-development-merketplace-industry-icon-1.webp",
     title: "Market Places",
     description:
       "Marketplace apps function as digital spaces for buyers and sellers to connect and conduct transactions. These apps provide a convenient platform to buy and sell a wide variety of products and services, all from your smartphone or tablet.",
