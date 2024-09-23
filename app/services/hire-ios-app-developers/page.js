11ddb6b7dcc7f5e00f7915b370e7b332d0a6309b
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-09-23 20:58:00 +0500

    added seo scripts in hire ios app dev page

--- app/services/hire-ios-app-developers/page.js
+++ app/services/hire-ios-app-developers/page.js
@@ -1,5 +1,6 @@
 import HireIosAppDevelopersPage from "@/components/services/HireIosAppDevelopers/HireIosAppDevelopersPage";
 import ServicesLayout from "@/layout/ServiceLayout";
+import Script from "next/script";
 import React from "react";
 
 export const metadata = {
@@ -11,7 +12,153 @@ export const metadata = {
 const page = () => {
   return (
     <div>
+      <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link
+          rel="canonical"
+          href="https://www.dignitestudios.com/services/hire-ios-app-developer"
+        />
+        <link
+          rel="alternate"
+          hreflang="en-ca"
+          href="https://www.dignitestudios.com/services/hire-ios-app-developer"
+        />
+        <link
+          rel="alternate"
+          hreflang="en-us"
+          href="https://www.dignitestudios.com/services/hire-ios-app-developer"
+        />
+        <link
+          rel="alternate"
+          hreflang="en-gb"
+          href="https://www.dignitestudios.com/services/hire-ios-app-developer"
+        />
+        <link
+          rel="alternate"
+          hreflang="x-default"
+          href="https://www.dignitestudios.com/services/hire-ios-app-developer"
+        />
+        <meta http-equiv="content-language" content="en-us" />
+        <meta http-equiv="content-language" content="en-ca" />
+        <meta http-equiv="content-language" content="en-gb" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow, max-snippet: 200" />
+        <meta name="bingbot" content="index, follow, max-snippet: 200 " />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Dignite Studios" />
+        <meta
+          property="og:title"
+          content="Hire iOS App Developers | Dignite Studios"
+        />
+        <meta
+          property="og:description"
+          content="iPhone app developers can develop applications in a mere time so hire an iOS app developer to reduce the risk patterns."
+        />
+        <meta
+          property="og:url"
+          content="https://www.dignitestudios.com/services/hire-ios-app-developer"
+        />
+        <meta
+          property="og:image"
+          content="https://www.dignitestudios.com/banner-cover-image-of-hire-ios-app-developer.webp"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Hire iOS App Developers" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Hire iOS App Developers | Dignite Studios"
+        />
+        <meta name="twitter:site" content="@dignitestudios" />
+        <meta
+          name="twitter:description"
+          content="iPhone app developers can develop applications in a mere time so hire an iOS app developer to reduce the risk patterns."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.dignitestudios.com/banner-cover-image-of-hire-ios-app-developer.webp"
+        />
+        <meta name="twitter:image:alt" content="Hire iOS App Developers" />
+      </head>
       <ServicesLayout page={<HireIosAppDevelopersPage />} />
+
+      <Script
+        type="application/ld+json"
+        id="webpage-schema"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebPage",
+            name: "Hire iOS App Developers | Dignite Studios",
+            url: "https://www.dignitestudios.com/services/hire-ios-app-developer",
+            description:
+              "iPhone app developers can develop applications in a mere time so hire an iOS app developer to reduce the risk patterns.",
+            thumbnailUrl:
+              "https://www.dignitestudios.com/banner-cover-image-of-hire-ios-app-developer.webp",
+          }),
+        }}
+      />
+
+      <Script
+        type="application/ld+json"
+        id="breadcrumb-schema-ios"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "BreadcrumbList",
+            itemListElement: [
+              {
+                "@type": "ListItem",
+                position: 1,
+                name: "Home Page",
+                item: "https://www.dignitestudios.com/",
+              },
+              {
+                "@type": "ListItem",
+                position: 2,
+                name: "iOS App Development Services",
+                item: "https://www.dignitestudios.com/services/android-app-development-services",
+              },
+              {
+                "@type": "ListItem",
+                position: 3,
+                name: "iOS App Design Services",
+                item: "https://www.dignitestudios.com/services/android-mobile-app-design-services",
+              },
+              {
+                "@type": "ListItem",
+                position: 4,
+                name: "Hire iOS App Developers",
+                item: "https://www.dignitestudios.com/services/hire-ios-app-developer",
+              },
+            ],
+          }),
+        }}
+      />
+
+      <Script
+        type="application/ld+json"
+        id="product-schema"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "http://schema.org",
+            "@type": "Product",
+            name: "Hire iOS App Developers",
+            aggregateRating: {
+              "@type": "AggregateRating",
+              ratingValue: "4.8",
+              worstRating: "1",
+              bestRating: "5",
+              reviewCount: "37",
+            },
+          }),
+        }}
+      />
     </div>
   );
 };
--- components/services/HireIosAppDevelopers/FAQS.jsx
+++ components/services/HireIosAppDevelopers/FAQS.jsx
@@ -95,6 +95,60 @@ const FAQS = () => {
           })}
         </div>
       </div>
+
+      <Script
+        type="application/ld+json"
+        id="faq-schema"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            mainEntity: [
+              {
+                "@type": "Question",
+                name: "How much does it cost to hire an iOS app developer?",
+                acceptedAnswer: {
+                  "@type": "Answer",
+                  text: "The developer cost depends on the project dimensions and proposition. Please fill out the form below to Hire iOS developers to create a user-centric app with modern techniques and approaches.",
+                },
+              },
+              {
+                "@type": "Question",
+                name: "How to hire an iOS developer?",
+                acceptedAnswer: {
+                  "@type": "Answer",
+                  text: "You can hire experienced iOS talents by analyzing their skills and proficiency, experience level, and knowledge of using modern technologies.",
+                },
+              },
+              {
+                "@type": "Question",
+                name: "How much do freelance iOS developers charge?",
+                acceptedAnswer: {
+                  "@type": "Answer",
+                  text: "To know the exact quote fill out the below form or schedule a call now and hire iOS developers.",
+                },
+              },
+              {
+                "@type": "Question",
+                name: "How much does it cost to develop an app for iOS?",
+                acceptedAnswer: {
+                  "@type": "Answer",
+                  text: "The cost for hiring a development company for crafting an iOS app can differ as per the specifications of your project. To know the exact quote fill out the form below to schedule a call with us.",
+                },
+              },
+              {
+                "@type": "Question",
+                name: "What does an iOS app developer do?",
+                acceptedAnswer: {
+                  "@type": "Answer",
+                  text: "iOS app developers build and craft unique Applications for your project. These developers develop the entire process and maintain the mobile application development tasks according to the development time frame.",
+                },
+              },
+            ],
+          }),
+        }}
+      />
     </section>
   );
 };
