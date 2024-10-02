9fb2ebdeee6dce1aa7ce2ace39336f5490c81755
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-03 02:36:34 +0500

    added a script in global layout

--- app/layout.js
+++ app/layout.js
@@ -33,6 +33,21 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
+
+        {/* <!-- Clarity tracking code for https://www.dignitestudios.com/ --> */}
+        <script>
+          (function(c,l,a,r,i,t,y)
+          {
+            (c[a] =
+              c[a] ||
+              function () {
+                (c[a].q = c[a].q || []).push(arguments);
+              })
+          }
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          )(window, document, "clarity", "script", "ocfggtesda");
+        </script>
       </head>
       <body className={inter.className}>
         <noscript>
