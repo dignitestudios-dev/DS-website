c9fa95b7ece4f1615d4abb34b0e48048dde7611d
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-04 00:32:32 +0500

    added meta viewport globally

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
         <Script
