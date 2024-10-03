df3945dddc8792e50caecdd51e3f203bea3f0419
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-04 03:29:08 +0500

    added some code in next config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
     loader: "custom",
     path: "https://dignitestudios.com/",
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply these headers to all routes
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
