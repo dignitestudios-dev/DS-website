eae2d7f85022299a52d6f49ed85d4ca404cc3cb4
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-04 02:35:12 +0500

    removed tags from global head

--- app/layout.js
+++ app/layout.js
@@ -11,11 +11,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="default-src 'self';"
-        />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
         <Script
