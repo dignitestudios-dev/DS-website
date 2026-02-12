93d04457011b5f594ffc01921b95154a649dc123
huzaifa-koder <huzaifa.tanveer@launchbox.pk>
2026-02-12 18:32:08 +0500

    all done

--- components/services/MobileAppDevelopmentSeo/MobileAppDevSeoHero.jsx
+++ components/services/MobileAppDevelopmentSeo/MobileAppDevSeoHero.jsx
@@ -10,7 +10,7 @@ const MobileAppDevSeoHero = () => {
   const { palette, setIsSidebarOpen, theme } = useContext(GlobalContext);
 
   return (
-    <main className="top-rated-bg bg-black w-full h-[90vh] md:h-[80vh] lg:h-screen flex flex-col justify-start items-start pt-28 md:pt-40 z-0 relative">
+    <main className="top-rated-bg bg-black w-full h-[90vh] md:h-[80vh] lg:h-screen flex flex-col justify-start items-start  z-0 relative">
       {theme === "light" ? (
         <div className="revolutionary-hero-overlay-light" />
       ) : (
